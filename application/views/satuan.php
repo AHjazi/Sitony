126389930517f0e258f1b1985e70e58b9b9aa42a
AHjazi <ahmdhjzi06@gmail.com>
2023-05-03 15:04:06 +0800

    first innit

---
+++ application/views/satuan.php
@@ -0,0 +1,53 @@
+<div class="card" style="margin-left:25px;margin-right:25px;margin-top:25px;">
+    <h5 class="card-header">Data Spare Part</h5>
+    <div class="table-responsive text-nowrap">
+        <td><a href="<?php echo base_url('tambahsatuan')?>"><button class="btn rounded-pill btn-primary"
+                    style="margin-left:20px"><i class='bx bxs-folder-plus'></i> Tambah
+                    Data</i></button></a></td>
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th>No</th>
+                    <th>Jenis Satuan</th>
+                    <th>Aksi</th>
+                </tr>
+            </thead>
+            <tbody class="table-border-bottom-0">
+                <tr>
+                    <td>1.</td>
+                    <td>Albert Cook</td>
+                    <td>
+                        <div class="dropdown">
+                            <button type="button" class="btn p-0 dropdown-toggle hide-arrow" data-bs-toggle="dropdown">
+                                <i class="bx bx-dots-vertical-rounded"></i>
+                            </button>
+                            <div class="dropdown-menu">
+                                <a class="dropdown-item" href="javascript:void(0);"><i class="bx bx-edit-alt me-1"></i>
+                                    Edit</a>
+                                <a class="dropdown-item" href="javascript:void(0);"><i class="bx bx-trash me-1"></i>
+                                    Delete</a>
+                            </div>
+                        </div>
+                    </td>
+                </tr>
+                <tr>
+                    <td>2.</td>
+                    <td>Pcs</td>
+                    <td>
+                        <div class="dropdown">
+                            <button type="button" class="btn p-0 dropdown-toggle hide-arrow" data-bs-toggle="dropdown">
+                                <i class="bx bx-dots-vertical-rounded"></i>
+                            </button>
+                            <div class="dropdown-menu">
+                                <a class="dropdown-item" href="javascript:void(0);"><i class="bx bx-edit-alt me-1"></i>
+                                    Edit</a>
+                                <a class="dropdown-item" href="javascript:void(0);"><i class="bx bx-trash me-1"></i>
+                                    Delete</a>
+                            </div>
+                        </div>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</div>
