dca1c693a747ec81dfc78982423192827fa74a58
Hilmanadi Yahya <hlmnyhya99@gmail.com>
2023-07-05 13:54:19 +0800

    add

--- application/controllers/BarangKeluar.php
+++ application/controllers/BarangKeluar.php
@@ -7,12 +7,15 @@ class BarangKeluar extends CI_Controller {
 	public function index()
 	{
 		$this->load->model('M_barangkeluar');
-		$data['barangkeluar'] = $this->M_barangkeluar->show_data();
+		$data['barangkeluar'] = $this->M_barangkeluar->insert_data_keluar();
+		$data['barangkeluar'] = $this->M_barangkeluar->show_data()->result();
 		$this->load->view('templates/header');
         $this->load->view('templates/sidebar');
 		$this->load->view('templates/navbar');
         $this->load->view('barangkeluar',$data);
         $this->load->view('templates/footer');
 	}
+
+	
 	
 }
--- application/models/M_barangkeluar.php
+++ application/models/M_barangkeluar.php
@@ -2,11 +2,39 @@
 
 class M_barangkeluar extends CI_Model
 {
+public function insert_data_keluar()
+{
+    $this->db->select('id_permintaan, nama_barang, jumlah, nama_pegawai');
+    $this->db->from('permintaan');
+    $data = $this->db->get()->result_array();
+
+    foreach ($data as $row) {
+        // Check if the data already exists in the barangkeluar table
+        $existingData = $this->db->get_where('barangkeluar', array(
+            'nama_barang' => $row['nama_barang'],
+            'jumlah' => $row['jumlah'],
+            'penerima' => $row['nama_pegawai'],
+        ))->row_array();
+
+        if (!$existingData) {
+            $insert_data = array(
+                'tgl_barangkeluar' => date('Y-m-d H:i:s'),
+                'nama_barang' => $row['nama_barang'],
+                'jumlah' => $row['jumlah'],
+                'penerima' => $row['nama_pegawai'],
+            );
+
+            $this->db->insert('barangkeluar', $insert_data);
+        }
+    }
+}
+
 
     public function show_data()
     {
-        return $this->db->get('barangkeluar')->result();
+        return $this->db->get('barangkeluar');
     }
+
 
     public function get_data($table){
         return $this->db->get($table);
